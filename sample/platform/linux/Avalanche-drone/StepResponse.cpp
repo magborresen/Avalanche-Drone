55fe48151bf1c54834e7b030d3319dd53dbcc899
Magnus Borresen <mabo@gomspace.com>
2020-11-10 23:12:23 +0100

    step response can now build

--- sample/platform/linux/Avalanche-drone/StepResponse.cpp
+++ sample/platform/linux/Avalanche-drone/StepResponse.cpp
@@ -2,67 +2,72 @@
 #include "StepResponse.hpp"
 
 using namespace DJI::OSDK;
-using namespace DJI::OSDK:Telemetry;
+using namespace DJI::OSDK::Telemetry;
 
 
-Telemetry::Quaternion quarternion;
+Telemetry::Quaternion quaternion;
+Telemetry::TimeStamp timeStamp;
 
 std::fstream myFile;
-std::string filename = "logFile.txt";
+std::string logFile = "logFile.txt";
 // Create a function to get the yaw that takes the vehicle as a parameter and a timeout parameter
-void getYaw (Vehicle* vehicle, int timeoutParamInMs)
+
+void clearFile()
+{
+    myFile.open(logFile,  std::ofstream::out | std::ofstream::trunc);
+    myFile.close();
+}
+
+void appendToFile(float yaw, uint32_t timeStamp)
+{
+	myFile.open(logFile, std::ios_base::app);
+	myFile << yaw << "," << timeStamp;
+	myFile << "\n";
+	myFile.close();
+}
+
+void getYaw(Vehicle* vehicle, int timeoutParamInMs)
 {
 	float yaw;
 	int elapsedTimeInMs = 0;
 	while(elapsedTimeInMs < timeoutParamInMs)
 	{
-		quarternion = vehicle->broadcast->getQuarternion();
-		yaw = 2*argsin(quarternion.q3);
-		appendToFile(yaw);
+		quaternion = vehicle->broadcast->getQuaternion();
+		timeStamp = vehicle->broadcast->getTimeStamp();
+		yaw = 2*asin(quaternion.q3);
+		appendToFile(yaw, timeStamp.time_ms);
 		usleep(5000);
 		elapsedTimeInMs += 5;
 	}
 }
 
 
-// Setup telemetry variables (see line 57 in the telemetry sample)
-
-
-
-
-// Setup broadcast frequency
-
-
-
-
-// Get the quartenion and convert to yaw
-
-
-
-
-
-// Create a function that can open and save data to a file that takes the yaw as the parameter
-void openFile()
-{
-    myfile.open(filename,  std::ofstream::out | std::ofstream::trunc);
-    myfile.close();
-}
-
-void appendToFile(float input)
-{
-	myFile.open(filename, std::ios_base::app);
-	myFile << input;
-	myFile << "; ";
-	myFile.close();
-}
-	
-
-
 // Create a function that gives the yaw a step response
 
-bool doStep()
+void doStep(Vehicle* vehicle)
 {
-	openFile();
-	WayPointInitSettings fdata;
+	float xCmd = 0;
+	float yCmd = 0;
+	float zCmd = 0;
+	float yawCmd = 150;
+	
+	float xPos = 0;
+	float yPos = 0;
+	float zPos = 2;
+	float yawPos = 180;
+	
+	int measuringTimeMs = 5000;
+	
+	clearFile();
+	std::cout << "Taking off...";
+	vehicle->control->takeoff();
+	std::cout << "TakeOff succeeded...";
+	std::cout << "Setting Yaw rate...";
+	vehicle->control->velocityAndYawRateCtrl(xCmd, yCmd, zCmd, yawCmd);
+	std::cout << "Yaw Rate set...";
+	std::cout << "Rotating";
+	vehicle->control->positionAndYawCtrl(xPos, yPos, zPos, yawPos);
+	getYaw(vehicle, measuringTimeMs);
+	
 	
 }
