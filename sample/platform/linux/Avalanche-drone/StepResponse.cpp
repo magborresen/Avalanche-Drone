7b15fdcd87cd977e6f18dda240b48cb4a77e977b
Magnus Borresen <mabo@gomspace.com>
2020-11-10 10:53:30 +0100

    added comments to work from

---
+++ sample/platform/linux/Avalanche-drone/StepResponse.cpp
@@ -0,0 +1,31 @@
+// Import relevant libraries from the SDK
+
+
+
+
+// Create a function to get the yaw that takes the vehicle as a parameter and a timeout parameter
+
+
+
+// Setup telemetry variables (see line 57 in the telemetry sample)
+
+
+
+
+// Setup broadcast frequency
+
+
+
+
+// Get the quartenion and convert to yaw
+
+
+
+
+
+// Create a function that can open and save data to a file that takes the yaw as the parameter
+
+
+
+
+// Create a function that gives the yaw a step response
