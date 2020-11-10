f2ebfa337067dbeb60d2218095bc9c83c0f38cc6
magnuskingo <mkch18@student.aau.dk>
2020-11-10 10:41:44 +0100

    flot bashar

---
+++ sample/platform/linux/Avalanche-drone/StepResponse.hpp
@@ -0,0 +1,21 @@
+/*
+    This header file is to make and monitor a 180 deg stepresponse around the yaw axis of the drone.
+    The header contains the functions needed to complete this.
+*/
+
+#ifndef STEP_RESPONSE_HPP
+#define STEP_RESPONSE_HPP
+
+// System includes
+#include <cmath>
+#include <vector>
+
+#include <iostream>
+#include <fstream>
+
+// DJI OSDK includes
+#include <dji_vehicle.hpp>
+#include <dji_telemetry.hpp>
+
+// Helpers
+#include <dji_linux_helpers.hpp>
