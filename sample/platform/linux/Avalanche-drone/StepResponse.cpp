6df0324d04ca4aaeae302f70007b82ca468d7d75
Magnus Borresen <mabo@gomspace.com>
2020-11-11 14:06:54 +0100

    added monitored takeoff

--- sample/platform/linux/Avalanche-drone/StepResponse.cpp
+++ sample/platform/linux/Avalanche-drone/StepResponse.cpp
@@ -45,6 +45,246 @@ void getYaw(Vehicle* vehicle, int timeoutParamInMs)
 	}
 }
 
+bool monitoredTakeoff(Vehicle* vehicle, int timeout)
+{
+  //@todo: remove this once the getErrorCode function signature changes
+  char func[50];
+  int  pkgIndex;
+
+  if (!vehicle->isM100() && !vehicle->isLegacyM600())
+  {
+    // Telemetry: Verify the subscription
+    ACK::ErrorCode subscribeStatus;
+    subscribeStatus = vehicle->subscribe->verify(timeout);
+    if (ACK::getError(subscribeStatus) != ACK::SUCCESS)
+    {
+      ACK::getErrorCodeMessage(subscribeStatus, func);
+      return false;
+    }
+
+    // Telemetry: Subscribe to flight status and mode at freq 10 Hz
+    pkgIndex                  = 0;
+    int       freq            = 10;
+    TopicName topicList10Hz[] = { TOPIC_STATUS_FLIGHT,
+                                  TOPIC_STATUS_DISPLAYMODE };
+    int  numTopic        = sizeof(topicList10Hz) / sizeof(topicList10Hz[0]);
+    bool enableTimestamp = false;
+
+    bool pkgStatus = vehicle->subscribe->initPackageFromTopicList(
+      pkgIndex, numTopic, topicList10Hz, enableTimestamp, freq);
+    if (!(pkgStatus))
+    {
+      return pkgStatus;
+    }
+    subscribeStatus = vehicle->subscribe->startPackage(pkgIndex, timeout);
+    if (ACK::getError(subscribeStatus) != ACK::SUCCESS)
+    {
+      ACK::getErrorCodeMessage(subscribeStatus, func);
+      // Cleanup before return
+      vehicle->subscribe->removePackage(pkgIndex, timeout);
+      return false;
+    }
+  }
+
+  // Start takeoff
+  ACK::ErrorCode takeoffStatus = vehicle->control->takeoff(timeout);
+  if (ACK::getError(takeoffStatus) != ACK::SUCCESS)
+  {
+    ACK::getErrorCodeMessage(takeoffStatus, func);
+    return false;
+  }
+
+  // First check: Motors started
+  int motorsNotStarted = 0;
+  int timeoutCycles    = 20;
+
+  if (!vehicle->isM100() && !vehicle->isLegacyM600())
+  {
+    while (vehicle->subscribe->getValue<TOPIC_STATUS_FLIGHT>() !=
+             VehicleStatus::FlightStatus::ON_GROUND &&
+           vehicle->subscribe->getValue<TOPIC_STATUS_DISPLAYMODE>() !=
+             VehicleStatus::DisplayMode::MODE_ENGINE_START &&
+           motorsNotStarted < timeoutCycles)
+    {
+      motorsNotStarted++;
+      usleep(100000);
+    }
+
+    if (motorsNotStarted == timeoutCycles)
+    {
+      std::cout << "Takeoff failed. Motors are not spinning." << std::endl;
+      // Cleanup
+      if (!vehicle->isM100() && !vehicle->isLegacyM600())
+      {
+        vehicle->subscribe->removePackage(0, timeout);
+      }
+      return false;
+    }
+    else
+    {
+      std::cout << "Motors spinning...\n";
+    }
+  }
+  else if (vehicle->isLegacyM600())
+  {
+    while ((vehicle->broadcast->getStatus().flight <
+            DJI::OSDK::VehicleStatus::FlightStatus::ON_GROUND) &&
+           motorsNotStarted < timeoutCycles)
+    {
+      motorsNotStarted++;
+      usleep(100000);
+    }
+
+    if (motorsNotStarted < timeoutCycles)
+    {
+      std::cout << "Successful TakeOff!" << std::endl;
+    }
+  }
+  else // M100
+  {
+    while ((vehicle->broadcast->getStatus().flight <
+            DJI::OSDK::VehicleStatus::M100FlightStatus::TAKEOFF) &&
+           motorsNotStarted < timeoutCycles)
+    {
+      motorsNotStarted++;
+      usleep(100000);
+    }
+
+    if (motorsNotStarted < timeoutCycles)
+    {
+      std::cout << "Successful TakeOff!" << std::endl;
+    }
+  }
+
+  // Second check: In air
+  int stillOnGround = 0;
+  timeoutCycles     = 110;
+
+  if (!vehicle->isM100() && !vehicle->isLegacyM600())
+  {
+    while (vehicle->subscribe->getValue<TOPIC_STATUS_FLIGHT>() !=
+             VehicleStatus::FlightStatus::IN_AIR &&
+           (vehicle->subscribe->getValue<TOPIC_STATUS_DISPLAYMODE>() !=
+              VehicleStatus::DisplayMode::MODE_ASSISTED_TAKEOFF ||
+            vehicle->subscribe->getValue<TOPIC_STATUS_DISPLAYMODE>() !=
+              VehicleStatus::DisplayMode::MODE_AUTO_TAKEOFF) &&
+           stillOnGround < timeoutCycles)
+    {
+      stillOnGround++;
+      usleep(100000);
+    }
+
+    if (stillOnGround == timeoutCycles)
+    {
+      std::cout << "Takeoff failed. Aircraft is still on the ground, but the "
+                   "motors are spinning."
+                << std::endl;
+      // Cleanup
+      if (!vehicle->isM100() && !vehicle->isLegacyM600())
+      {
+        vehicle->subscribe->removePackage(0, timeout);
+      }
+      return false;
+    }
+    else
+    {
+      std::cout << "Ascending...\n";
+    }
+  }
+  else if (vehicle->isLegacyM600())
+  {
+    while ((vehicle->broadcast->getStatus().flight <
+            DJI::OSDK::VehicleStatus::FlightStatus::IN_AIR) &&
+           stillOnGround < timeoutCycles)
+    {
+      stillOnGround++;
+      usleep(100000);
+    }
+
+    if (stillOnGround < timeoutCycles)
+    {
+      std::cout << "Aircraft in air!" << std::endl;
+    }
+  }
+  else // M100
+  {
+    while ((vehicle->broadcast->getStatus().flight !=
+            DJI::OSDK::VehicleStatus::M100FlightStatus::IN_AIR_STANDBY) &&
+           stillOnGround < timeoutCycles)
+    {
+      stillOnGround++;
+      usleep(100000);
+    }
+
+    if (stillOnGround < timeoutCycles)
+    {
+      std::cout << "Aircraft in air!" << std::endl;
+    }
+  }
+
+  // Final check: Finished takeoff
+  if (!vehicle->isM100() && !vehicle->isLegacyM600())
+  {
+    while (vehicle->subscribe->getValue<TOPIC_STATUS_DISPLAYMODE>() ==
+             VehicleStatus::DisplayMode::MODE_ASSISTED_TAKEOFF ||
+           vehicle->subscribe->getValue<TOPIC_STATUS_DISPLAYMODE>() ==
+             VehicleStatus::DisplayMode::MODE_AUTO_TAKEOFF)
+    {
+      sleep(1);
+    }
+
+    if (!vehicle->isM100() && !vehicle->isLegacyM600())
+    {
+      if (vehicle->subscribe->getValue<TOPIC_STATUS_DISPLAYMODE>() !=
+            VehicleStatus::DisplayMode::MODE_P_GPS ||
+          vehicle->subscribe->getValue<TOPIC_STATUS_DISPLAYMODE>() !=
+            VehicleStatus::DisplayMode::MODE_ATTITUDE)
+      {
+        std::cout << "Successful takeoff!\n";
+      }
+      else
+      {
+        std::cout
+          << "Takeoff finished, but the aircraft is in an unexpected mode. "
+             "Please connect DJI GO.\n";
+        vehicle->subscribe->removePackage(0, timeout);
+        return false;
+      }
+    }
+  }
+  else
+  {
+    float32_t                 delta;
+    Telemetry::GlobalPosition currentHeight;
+    Telemetry::GlobalPosition deltaHeight =
+      vehicle->broadcast->getGlobalPosition();
+
+    do
+    {
+      sleep(4);
+      currentHeight = vehicle->broadcast->getGlobalPosition();
+      delta         = fabs(currentHeight.altitude - deltaHeight.altitude);
+      deltaHeight.altitude = currentHeight.altitude;
+    } while (delta >= 0.009);
+
+    std::cout << "Aircraft hovering at " << currentHeight.altitude << "m!\n";
+  }
+
+  // Cleanup
+  if (!vehicle->isM100() && !vehicle->isLegacyM600())
+  {
+    ACK::ErrorCode ack = vehicle->subscribe->removePackage(pkgIndex, timeout);
+    if (ACK::getError(ack))
+    {
+      std::cout
+        << "Error unsubscribing; please restart the drone/FC to get back "
+           "to a clean state.\n";
+    }
+  }
+
+  return true;
+}
+
 
 // Create a function that gives the yaw a step response
 
@@ -65,7 +305,7 @@ void doStep(Vehicle* vehicle)
 	
 	clearFile();
 	std::cout << "Taking off..." << "\n";
-	vehicle->control->takeoff();
+	monitoredTakeoff(vehicle);
 	std::cout << "TakeOff succeeded..." << "\n";
 	usleep(1000*1000);
 	std::cout << "Setting Yaw rate..." << "\n";
