50dbba3c4d37f64a87ecf3d6591b91e76d04d79f
Magnus Borresen <mabo@gomspace.com>
2020-11-12 15:17:38 +0100

    new file try

--- sample/platform/linux/Avalanche-drone/StepResponse.cpp
+++ sample/platform/linux/Avalanche-drone/StepResponse.cpp
@@ -460,7 +460,9 @@ bool moveByPositionOffset(Vehicle *vehicle, float xOffsetDesired,
   {
     zCmd = currentBroadcastGP.height + zOffsetDesired;
   }
-
+	FILE * pFile;
+	
+	pFile = fopen("logFile.txt", "w");
   //! Main closed-loop receding setpoint position control
   while (elapsedTimeInMs < timeoutInMilSec)
   {
@@ -472,9 +474,7 @@ bool moveByPositionOffset(Vehicle *vehicle, float xOffsetDesired,
 	
 	timeStamp = vehicle->broadcast->getTimeStamp();
 	
-	FILE * pFile;
 	
-	pFile = fopen("logFile.txt", "w");
 	
 	fprintf(pFile, "%f, %d \n", yawInRad / DEG2RAD, timeStamp.time_ms);
 	
