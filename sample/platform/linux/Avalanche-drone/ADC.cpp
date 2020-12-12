4cc332795b722b17bb02d7ab23737629b13317e8
mikkelfb <mbengt17@student.aau.dk>
2020-12-12 17:16:11 +0100

    Update ADC.cpp

--- sample/platform/linux/Avalanche-drone/ADC.cpp
+++ sample/platform/linux/Avalanche-drone/ADC.cpp
@@ -36,7 +36,7 @@ int startADCSPI() {
 //readADC takes in number of samples to perform in a row and return the result in a uint16 vector
 vector<uint16_t> readADC(int numberOfSamples) {
 	vector<uint16_t> result;
-	char buf[numberOfSamples*2] = {0};
+	char buf[2] = {0};
 
 	uint32_t RXCnt=0;
 
@@ -54,7 +54,7 @@ vector<uint16_t> readADC(int numberOfSamples) {
 		bcm2835_peri_set_bits(paddr, BCM2835_SPI0_CS_TA, BCM2835_SPI0_CS_TA);
 		while(((bcm2835_peri_read(paddr) & BCM2835_SPI0_CS_RXD))&&( RXCnt < 2 ))
 		{
-			rbuf[RXCnt] = bcm2835_peri_read_nb(fifo);
+			buf[RXCnt] = bcm2835_peri_read_nb(fifo);
 			RXCnt++;
 		}
 		/* Set TA = 0 */
